126021049171e369452b2a15c7106ebffe84d570
Elizabeth Tabler <beth@beforewego.net>
2013-12-26 14:36:35 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Romeo::Application.config.secret_key_base = 'f18bc17b767a30f3effb5ce1d1409dbb2d278efe6912655fc7149e98e74c758ecfaea9beaacefc1f5d49ab57a9e3cf507a697ca0e8af6e75f1909cbdb2315ea0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Romeo::Application.config.session_store :cookie_store, key: '_romeo_session'
